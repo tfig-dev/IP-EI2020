99844b0145ff89fbcee703d9cdd03e75dfa697dd
buceta <tiagofr.figueiredo@gmail.com>
2020-10-31 22:43:37 +0000

    Added ex1

---
+++ class02/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+void main (void) {
+    double raio, area, per;
+    printf ("Find Area: ");
+    scanf ("%lf", &raio);
+    area = M_PI * (raio * raio);
+    per = 2 * M_PI * raio;
+    printf ("Area: %lf\n",  area);
+    printf ("Diametro: %lf\n", raio*2);
+    printf ("Perímetro: %lf\n", per);
+}
+
